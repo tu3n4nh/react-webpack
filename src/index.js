021c20fed577852244dba0521c160383a013e7b1
tu3n4nh <buituananh3423@gmail.com>
2024-04-25 09:55:03 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as ReactDOM from 'react-dom';
+
+// Tạo component App
+function App() {
+    return(
+        <div>
+            <h1>Xin chào!</h1>
+            11234
+        </div>
+    )
+}
+
+// Render component App vào #root element
+ReactDOM.render(<App />, document.getElementById("root"));
